171def9cb1d9db739cb48437720e7c28b06987da
Mike Mind <39806226+webwealthme@users.noreply.github.com>
2018-12-27 15:49:40 +0200

    uploaded

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# frelancer_time_tracker
-Time tracker for freelancer
+## Freelance Timetracker
+
+Freelance Timetracker is a free and super-simple web-based timetracking system. All the information is stored in a JSON file so no database is needed.
+
+
+### 10 second Setup
+
+1. Copy the files to your web server
+2. You are done!
+
+#### Quick notes
+
+Make sure PHP can create/edit files in that path. You should use Freelance Timetracker locally since there is no password protection, everyone could access it and play around with it. Also, make sure not to open several tabs of it since you may overwrite the database. No configuration is necessary although you can customize some things such as saving interval, default rate, currency, database extension and a few other things.
+
+
+### How to use
+
+Click the checkbox to start timing a task, edit any fields by clicking on them.
+
+
+### Contents
+
+The code is just one PHP, CSS, JS and JSON file.
+
+
+### How it works
+
+- Twitter Bootstrap gives it styling
+- jQuery/AJAX sends the information to the back-end (every 10 seconds by default)
+- PHP stores everything in a JSON file
+
+This setup makes it extremely easy to install Freelance Timetracker with just a copy-paste and start using straight away. It also makes backing up your stuff super-easy.
+
+
+
+
---
+++ index.php
@@ -0,0 +1,144 @@
+<?php
+/*!
+ * 
+ ______             _                      
+|  ____|           | |                     
+| |__ _ __ ___  ___| | __ _ _ __   ___ ___ 
+|  __| '__/ _ \/ _ \ |/ _` | '_ \ / __/ _ \
+| |  | | |  __/  __/ | (_| | | | | (_|  __/
+|_|  |_|  \___|\___|_|\__,_|_| |_|\___\___|
+ _______ _                _                  _             
+|__   __(_)              | |                | |            
+   | |   _ _ __ ___   ___| |_ _ __ __ _  ___| | _____ _ __ 
+   | |  | | '_ ` _ \ / _ \ __| '__/ _` |/ __| |/ / _ \ '__|
+   | |  | | | | | | |  __/ |_| | | (_| | (__|   <  __/ |   
+   |_|  |_|_| |_| |_|\___|\__|_|  \__,_|\___|_|\_\___|_|   
+   
+
+ */
+
+/**
+ * Default Settings
+ * Set your default settings here
+ */
+$settings = array(
+	'rate' => 35, // default rate per hour
+	'currency' => '£',
+	'filename' => 'timetracker', // name of the database
+	'tasksno' => 10, // TODO: Number of tasks to show
+	'saveinterval' => 10 // save every N seconds
+);
+
+/**
+ * Save database
+ */
+if (isset($_GET['action'])) {
+	if ($_GET['action']=='save' && (strlen($_POST['json'])>2)) {
+		$filename = $settings['filename'].".json";
+		$filehandle = fopen($filename, 'w') or die("Error: Can't create or save files, please modify folder permissions");
+		$fwrite = fwrite($filehandle, stripslashes($_POST['json']));
+		fclose($filehandle);
+		echo $fwrite;
+		die();
+	}
+}
+
+/**
+ * Read database
+ */
+$data = '';
+if (file_exists($settings['filename'].".json")) {
+	$data = file_get_contents($settings['filename'].".json");
+}
+
+// If no database then generate a blank one
+if (strlen($data)<3) {
+	$data = '{';
+	for ($i=0; $i < $settings['tasksno']; $i++) { 
+		$data .= '"'.$i.'":{"date":"","client":"","task":"","rate":'.$settings['rate'].',"total":0,"desc":"","timed":0}';
+		if ($i < $settings['tasksno']-1) {$data .= ',';}
+	}
+	$data .= '}';
+}
+
+/**
+ * HTML
+ */
+?><!doctype html>
+<head>
+	<title>Freelance Timetracker</title>
+	<meta charset="utf-8">
+	<link href="bootstrap.min.css" rel="stylesheet">
+	<style>
+	body {padding-top:60px}
+	input[type="text"],input[type="checkbox"]{margin-bottom:0}
+	input[type=text]{background:transparent;border:none;box-shadow:none}
+	input[type=text]:focus{background:#fff}
+	input.client,
+	input.desc{font-size:80%}
+	input.total{font-weight:bold}
+	td{transition: all 1s ease-in;-webkit-transition: all 1s ease-in;}
+	</style>
+</head>
+<body>
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="brand" href="?">Timetracker</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav pull-right">
+						<li>
+							<form class="navbar-form">
+								<button id="save" class="btn btn-success">Save now</a>
+							</form>
+						</li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+	</div>
+	
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				
+				<table id="table" class="table table-striped">
+					<thead>
+						<tr>
+							<th></th>
+							<th>Date</th>
+							<th>Project</th>
+							<th>Task</th>
+							<th>Rate</th>
+							<th>Total</th>
+							<th>Notes</th>
+							<th>Time</th>
+						</tr>
+					</thead>
+					<tbody></tbody>
+				</table>
+
+				<hr>
+
+			
+			</div><!--span12-->
+		</div><!--row-->
+	</div>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+<script>
+var t = {
+	config: {
+		currency: '<?=$settings['currency']?>',
+		rate: '<?=number_format($settings['rate'], 2)?>',
+		savenext: <?=$settings['saveinterval']*1000?>,
+		savedefault: <?=$settings['saveinterval']*1000?>,
+		initialdata: '<?=$data?>'
+	}
+};
+</script>
+<script src="timetracker.js"></script>
+
+</body>
+</html>
